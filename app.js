f141ef8ca4a3706b72e5a58a1e90509610ebb55d
TOTALPLAY\ecarballo <>
2023-06-05 17:13:35 -0600

    agregamos morgan para enviar logs a un webhook

--- app.js
+++ app.js
@@ -1,30 +1,19 @@
-
 require("dotenv").config();
 const express = require ("express"); 
 const cors = require ("cors"); 
 const dbConnect = require ('./config/mongo'); 
 const app = express(); 
+//const morganBody = require("morgan-body");
 //const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("storage"));
-/**
- * @Const port: =>
- * 
- */
+
 const port = process.env.PORT || 3000;
-/***
- * 
- * @variable Routes => 
- * 
- */
+
 app.use("/api", require("./routes"));
 
-
-/**
- * @Variable App => 
- */
 app.listen(port , () => {
     console.log( ` Tu app esta lista por el puerto http://localhost:${port}`);
 })

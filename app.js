481cb0ee67626f1c99e0440eff0b27c6ef607c27
TOTALPLAY\ecarballo <>
2023-06-07 13:19:24 -0600

    generamos una conexion ala base de datos PG

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ const express = require ("express");
 const cors = require ("cors"); 
 const dbConnect = require ('./config/mongo'); 
 const app = express(); 
+const { Client } = require('pg');
+
+const connectionData = {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'Dev',
+    password: '12345dev',
+    port: 5432
+}
+
+  const client = new Client(connectionData);
+  client.connect();
+  client.query('SELECT * FROM conf_empresa_estatus').then(response => {
+          console.log(response.rows);
+          client.end();
+      }).catch(err => { client.end();});
+
 //const morganBody = require("morgan-body");
 //const port = 3000;
 
